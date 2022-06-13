6a0995eb8f9c5a16da3a27ceddfa8ba6900bd50c
Davis Chou <lxdfs58@gmail.com>
2022-06-14 05:13:01 +0800

    first commit

---
+++ BalancingCar.ino
@@ -0,0 +1,206 @@
+// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of your project
+#include "I2Cdev.h"
+#include <PID_v1.h>
+#include "MPU6050_6Axis_MotionApps20.h"
+//#include "MPU6050.h" // not necessary if using MotionApps include file
+#include "TBMotorController.h"
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
+// AD0 high = 0x69
+MPU6050 mpu;
+//MPU6050 mpu(0x69); // <-- use for AD0 high
+
+#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
+#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+#define MIN_ABS_SPEED 30
+bool blinkState = false;
+const double M_PI=3.1415926535897932384626433832795028841950;
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+
+// ================================================================
+// ===               INTERRUPT DETECTION ROUTINE                ===
+// ================================================================
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+// ================================================================
+// ===                    Motor & PID setup                     ===
+// ================================================================
+
+const int AIN1 = 8;   // 控制輸入A1
+const int AIN2 = 7;   // 控制輸入A2
+const int BIN1 = 10;  // 控制輸入B1
+const int BIN2 = 11;   // 控制輸入B2
+const int PWMA = 6;
+const int PWMB = 5;
+const int STBY = 9;  // 「待機」控制接Arduino的11腳
+
+double motorSpeedFactorLeft = 0.4;
+double motorSpeedFactorRight = 0.4;
+TBMotorController motorController(PWMA, AIN1, AIN2, PWMB, BIN1, BIN2, motorSpeedFactorLeft, motorSpeedFactorRight);
+//PID
+double originalSetpoint = 180;
+double setpoint = originalSetpoint;
+double movingAngleOffset = 0.1;
+double input, output;
+
+//adjust these values to fit your own design
+double Kp = 40;   
+double Kd = 1.5;
+double Ki = 0;
+PID pid(&input, &output, &setpoint, Kp, Ki, Kd, DIRECT);
+
+int speed;
+
+// ================================================================
+// ===                      INITIAL SETUP                       ===
+// ================================================================
+
+void setup() {
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    // initialize serial communication
+    // (115200 chosen because it is required for Teapot Demo output, but it's
+    // really up to you depending on your project)
+    Serial.begin(115200);
+    while (!Serial); // wait for Leonardo enumeration, others continue immediately
+
+    // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3V or Arduino
+    // Pro Mini running at 3.3V, cannot handle this baud rate reliably due to
+    // the baud timing being too misaligned with processor ticks. You must use
+    // 38400 or slower in these cases, or use some kind of external separate
+    // crystal solution for the UART timer.
+
+    // initialize device
+    Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+    pinMode(INTERRUPT_PIN, INPUT);
+
+    // verify connection
+    Serial.println(F("Testing device connections..."));
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+    // wait for ready
+    Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+    while (Serial.available() && Serial.read()); // empty buffer
+    while (!Serial.available());                 // wait for data
+    while (Serial.available() && Serial.read()); // empty buffer again
+
+    // load and configure the DMP
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // Calibration Time: generate offsets and calibrate our MPU6050
+        mpu.CalibrateAccel(6);
+        mpu.CalibrateGyro(6);
+        mpu.PrintActiveOffsets();
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+        Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+        Serial.println(F(")..."));
+        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+    //setup PID
+    pid.SetMode(AUTOMATIC);
+    pid.SetSampleTime(10);
+    pid.SetOutputLimits(-255, 255); 
+    // configure LED for output
+    pinMode(LED_PIN, OUTPUT);
+    // set STANDBY pin HIGH
+    pinMode(STBY, OUTPUT);
+    digitalWrite(STBY, HIGH);
+}
+
+
+
+// ================================================================
+// ===                    MAIN PROGRAM LOOP                     ===
+// ================================================================
+
+void loop() {
+  // if programming failed, don't try to do anything
+  if (!dmpReady) return;
+  // read a packet from FIFO
+  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet 
+    // display Euler angles in degrees
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    Serial.print("ypr\t");
+    Serial.print(ypr[0] * 180/M_PI);
+    Serial.print("\t");
+    Serial.print(ypr[1] * 180/M_PI);
+    Serial.print("\t");
+    Serial.println(ypr[2] * 180/M_PI);
+      
+    input = ypr[1] * 180/M_PI + 180;
+    // blink LED to indicate activity
+    blinkState = !blinkState;
+    digitalWrite(LED_PIN, blinkState);
+  }
+  pid.Compute();
+  speed = motorController.move(output, MIN_ABS_SPEED);
+  Serial.println(output);
+}
---
+++ TBMotorController.h
@@ -0,0 +1,97 @@
+class TBMotorController
+{
+protected:
+    int _ena, _in1, _in2, _enb, _in3, _in4;
+    int _currentSpeed;
+    double _motorAConst, _motorBConst;
+public:
+    TBMotorController(int ena, int in1, int in2, int enb, int in3, int in4, double motorAConst, double motorBConst);
+    void move(int leftSpeed, int rightSpeed, int minAbsSpeed);
+    void move(int speed);
+    int move(int speed, int minAbsSpeed);
+    void turnLeft(int speed, bool kick);
+    void turnRight(int speed, bool kick);
+    void stopMoving();
+};
+
+TBMotorController::TBMotorController(int ena, int in1, int in2, int enb, int in3, int in4, double motorAConst, double motorBConst)
+{
+    _motorAConst = motorAConst;
+    _motorBConst = motorBConst;
+    
+	_ena = ena;
+	_in1 = in1;
+	_in2 = in2;
+	_enb = enb;
+	_in3 = in3;
+	_in4 = in4;
+	
+	pinMode(_ena, OUTPUT);
+	pinMode(_in1, OUTPUT);
+	pinMode(_in2, OUTPUT);
+    
+	pinMode(_enb, OUTPUT);
+	pinMode(_in3, OUTPUT);
+	pinMode(_in4, OUTPUT);
+}
+
+int TBMotorController::move(int speed, int minAbsSpeed)
+{
+    int direction = 1;
+    
+    if (speed < 0)
+    {
+        direction = -1;
+        
+        speed = min(speed, -1*minAbsSpeed);
+        speed = max(speed, -255);
+    }
+    else
+    {
+        speed = max(speed, minAbsSpeed);
+        speed = min(speed, 255);
+    }
+    
+    // if (speed == _currentSpeed) return -1;
+    
+    int realSpeed = max(minAbsSpeed, abs(speed));
+    digitalWrite(_in1, speed > 0 ? HIGH : LOW);
+    digitalWrite(_in2, speed > 0 ? LOW : HIGH);
+    digitalWrite(_in3, speed > 0 ? HIGH : LOW);
+    digitalWrite(_in4, speed > 0 ? LOW : HIGH);
+    analogWrite(_ena, realSpeed * _motorAConst);
+    analogWrite(_enb, realSpeed * _motorBConst);
+    
+    _currentSpeed = direction * realSpeed;
+    return realSpeed;
+}
+
+
+void TBMotorController::move(int speed)
+{
+    if (speed == _currentSpeed) return;
+    
+    if (speed > 255) speed = 255;
+    else if (speed < -255) speed = -255;
+    
+    digitalWrite(_in1, speed > 0 ? HIGH : LOW);
+    digitalWrite(_in2, speed > 0 ? LOW : HIGH);
+    digitalWrite(_in3, speed > 0 ? HIGH : LOW);
+    digitalWrite(_in4, speed > 0 ? LOW : HIGH);
+    analogWrite(_ena, abs(speed) * _motorAConst);
+    analogWrite(_enb, abs(speed) * _motorBConst);
+    
+    _currentSpeed = speed;
+}
+
+void TBMotorController::stopMoving()
+{
+    digitalWrite(_in1, LOW);
+    digitalWrite(_in2, LOW);
+    digitalWrite(_in3, LOW);
+    digitalWrite(_in4, LOW);
+    digitalWrite(_ena, HIGH);
+    digitalWrite(_enb, HIGH);
+    
+    _currentSpeed = 0;
+}
